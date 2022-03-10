6ff7c0f5f29f07561778372c19361fc26d9c3dc0
TochigiV <bailey.brownie60@gmail.com>
2022-03-10 13:05:48 -0500

    Start initial work on macOS port

--- apps/openmw-mp/Script/Types.hpp
+++ apps/openmw-mp/Script/Types.hpp
@@ -99,7 +99,7 @@ struct CallbackIdentity
 struct ScriptFunctionPointer : public ScriptIdentity
 {
     void *addr;
-#if (!defined(__clang__) && defined(__GNUC__))
+#if ((!defined(__clang__) && defined(__GNUC__)) || defined(__APPLE__))
     template<typename R, typename... Types>
     constexpr ScriptFunctionPointer(Function<R, Types...> addr) : ScriptIdentity(addr), addr((void*)(addr)) {}
 #else
